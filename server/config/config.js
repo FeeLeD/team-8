7b9729b71f584ad870bbf13de591778cc96b4500
FeeLeD <feeled9@gmail.com>
2020-06-25 00:07:46 +0500

    config changes

--- server/config/config.js
+++ server/config/config.js
@@ -1,22 +1,22 @@
 const config = {
-    env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 5000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: 'mongodb+srv://user:QgOVTyn4wOcG8MbV@messenger-2geth.mongodb.net/test?retryWrites=true&w=majority',
-    smtpConfig: {
-      service: 'gmail',
-      host: 'smtp.gmail.com',
-      secure: false,
-      port: 25,
-      auth: {
-          user: "dreamteammessenger@gmail.com",
-          pass: "TBH-qG9-d7r-zi81"
-      },
-      tls: {
-          rejectUnauthorized: false
-      }
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 5000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: process.env.MONGO_URI,
+  smtpConfig: {
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    secure: false,
+    port: 25,
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.MAIL_PASSWORD
+    },
+    tls: {
+      rejectUnauthorized: false
     }
-
   }
-  
+
+}
+
 export default config
